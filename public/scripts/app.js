3ea0752c263b6bf7d4b469ec50e53dbf43d7f317
kbbushman <k.bushman@yahoo.com>
2016-09-24 23:43:48 -0700

    adds console log to index ajax request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,27 +19,29 @@ $(document).ready(function(){
 		error: showError
 	});
 
-	// helper function to render all posts to view
-	// note: we empty and re-render the collection each time our post data changes
-	function render() {
-		// remove existing movies from view
-		$moviesList.empty();
-
-		// pass `allMovies` into the template function
-		var moviesHtml = template({ movies: allMovies });
-
-		// append html to the view
-		$moviesList.append(moviesHtml);
-	};
-
-	function showSuccess(json) {
-		allMovies = json;
-		render();
-	};
-
-	function showError(e) {
-		console.log('Error rendering movies...');
-		$('#movies-list').text('Unable to show movies...');
-	};
-
 });
+
+
+// helper function to render all posts to view
+// note: we empty and re-render the collection each time our post data changes
+function render() {
+	// remove existing movies from view
+	$moviesList.empty();
+
+	// pass `allMovies` into the template function
+	var moviesHtml = template({ movies: allMovies });
+
+	// append html to the view
+	$moviesList.append(moviesHtml);
+};
+
+function showSuccess(json) {
+	allMovies = json;
+	console.log("Got all movies");
+	render();
+};
+
+function showError(e) {
+	console.log('Error rendering movies...');
+	$('#movies-list').text('Unable to show movies...');
+};
