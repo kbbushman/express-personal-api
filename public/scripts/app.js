ab7ce93427941c147f49ae7654d4873ccd489d99
kbbushman <k.bushman@yahoo.com>
2016-09-24 23:25:19 -0700

    restores html and app js after troubleshooting

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,40 +6,40 @@ var allMovies = [];
 
 $(document).ready(function(){
 
-	// $moviesList = $('#movies-list');
+	$moviesList = $('#movies-list');
 
-	// // compile handlebars template
-	// var source = $('#movies-template').html();
-	// template = Handlebars.compile(source);
+	// compile handlebars template
+	var source = $('#movies-template').html();
+	template = Handlebars.compile(source);
 
-	// $.ajax({
-	// 	method: 'GET',
-	// 	url: '/api/movies',
-	// 	success: showSuccess,
-	// 	error: showError
-	// });
+	$.ajax({
+		method: 'GET',
+		url: '/api/movies',
+		success: showSuccess,
+		error: showError
+	});
 
-	// // helper function to render all posts to view
-	// // note: we empty and re-render the collection each time our post data changes
-	// function render() {
-	// 	// remove existing movies from view
-	// 	$moviesList.empty();
+	// helper function to render all posts to view
+	// note: we empty and re-render the collection each time our post data changes
+	function render() {
+		// remove existing movies from view
+		$moviesList.empty();
 
-	// 	// pass `allMovies` into the template function
-	// 	var moviesHtml = template({ movies: allMovies });
+		// pass `allMovies` into the template function
+		var moviesHtml = template({ movies: allMovies });
 
-	// 	// append html to the view
-	// 	$moviesList.append(moviesHtml);
-	// };
+		// append html to the view
+		$moviesList.append(moviesHtml);
+	};
 
-	// function showSuccess(json) {
-	// 	allMovies = json;
-	// 	render();
-	// };
+	function showSuccess(json) {
+		allMovies = json;
+		render();
+	};
 
-	// function showError(e) {
-	// 	console.log('Error rendering movies...');
-	// 	$('#movies-list').text('Unable to show movies...');
-	// };
+	function showError(e) {
+		console.log('Error rendering movies...');
+		$('#movies-list').text('Unable to show movies...');
+	};
 
 });
